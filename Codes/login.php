509d1ba6a8499d30db651e76966c3d2bb075e6a7
AJBaloch112 <230048528@aston.ac.uk>
2024-03-28 12:41:46 +0000

    update

--- Codes/login.php
+++ Codes/login.php
@@ -1,6 +1,8 @@
 <?php
+session_start();
 // VerifyUser.php
 
+// Database connection details
 $host = 'localhost';
 $username = 'u-230064687';
 $password = 'UfWsjkbw4Ux0G1u';
@@ -14,34 +16,34 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get user input
-$email = $_POST['email'];
-$password = $_POST['password'];
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
 
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+if (!empty($email) && !empty($password)) {
+    $query = "SELECT * FROM user WHERE Email = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-$query = "SELECT * FROM user WHERE Email = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows > 0) {
-
-    $user = $result->fetch_assoc();
-
-    // Verify the password
-    if (password_verify($password, $user['Password'])) {
-        echo "Login successful!";
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['Password'])) {
+            $_SESSION['user_id'] = $user['UserID'];
+            $_SESSION['firstName'] = $firstName;  
+            $_SESSION['logged_in'] = true;
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            echo "Invalid password!";
+        }
     } else {
-        // Password is incorrect
-        echo "Invalid password!";
+        echo "User not found!";
     }
-} else {
-    // User with the given email doesn't exist
-    echo "User not found!";
-}
 
-$stmt->close();
-$conn->close();
+    $stmt->close();
+    $conn->close();
+} else {
+    echo "Email or Password is not set";
+}
 ?>
